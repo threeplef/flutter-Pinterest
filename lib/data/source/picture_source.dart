8e19defdb86971d63ae418e8f1564897c7aac88d
minhaneul <onlynforever@gmail.com>
2022-08-23 19:39:51 +0900

    Add: PhotoApi 테스트 코드 작성

--- lib/data/source/picture_source.dart
+++ lib/data/source/picture_source.dart
@@ -2,32 +2,26 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:json_test/data/model/photo.dart';
+
+import '../model/photo.dart';
 
 class PhotoApi {
-  PhotoApi() {
-    fetchImages('');
-  }
+  final http.Client _client;
 
-  final _imageStreamController = StreamController<List<Photo>>();
-
-  Stream<List<Photo>> get imagesStream => _imageStreamController.stream;
-
-  void fetchImages(String query) async {
-    List<Photo> imageList = await getImages(query);
-    _imageStreamController.add(imageList);
-  }
+  PhotoApi({http.Client? client}) : _client = (client ?? http.Client());
 
   Future<List<Photo>> getImages(String query) async {
     Uri url = Uri.parse(
         'https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo');
 
-    http.Response response = await http.get(url);
+    http.Response response = await _client.get(url);
+    print('Response status: ${response.statusCode}');
 
     String jsonString = response.body;
 
     Map<String, dynamic> json = jsonDecode(jsonString);
-    Iterable hits = json['hits'];
-    return hits.map((e) => Photo.fromJson(e)).toList();
+    List<dynamic> hits = json['hits'];
+    List<Photo> items = hits.map((e) => Photo.fromJson(e)).toList();
+    return items;
   }
 }
